f6d31a0d48b1383de1c414d8977de7aede629e7b
Jaxer24 <143577988+Jaxer24@users.noreply.github.com>
2026-01-11 06:19:26 +0000

    added data analytics tracker to see user web trafick

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/app/App";
+import { Analytics } from "@vercel/analytics/next"
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
