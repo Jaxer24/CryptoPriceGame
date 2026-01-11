64c51cd3dab94ed0a5c97f38a0cffbe6baedf5e7
Jaxer24 <143577988+Jaxer24@users.noreply.github.com>
2026-01-11 06:20:57 +0000

    fixing data analytics tracker

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/app/App";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
